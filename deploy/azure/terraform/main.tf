0faa4612f14b10125838d9870f5f1d15d294570c
goodchang77 <goodchang77@gmail.com>
2026-08-15 23:50:52 +0800

    deploy: Premium SSD for VM (better Postgres IO)

--- deploy/azure/terraform/main.tf
+++ deploy/azure/terraform/main.tf
@@ -142,7 +142,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
     disk_size_gb         = 64
   }
 
